7ccb712a7338fd5fa03893cfda4eb8224446b79a
gavrilova-spk <71066561+gavrilova-spk@users.noreply.github.com>
2020-10-10 18:02:52 +0300

    выполнено v0.5

--- Hw_2/Hw_2/Hw_2/Program.cs
+++ Hw_2/Hw_2/Hw_2/Program.cs
@@ -8,16 +8,23 @@ namespace Hw_2
 {
     class Program
     {
-        static double Check(ref string str)  //Проверка на десятичное число, замена точки на запятую
+        static double Check(ref string str, int n = 0)  //Проверка на десятичное число, замена точки на запятую
         {
             double num;
-            //int i = str.IndexOf('.');/////////////
             str = str.Replace('.', ',');
 
             if (!double.TryParse(str, out num))
             {
                 Console.WriteLine("Вводить надо число. Конец программы.");
+                Console.ReadKey();
                 Environment.Exit(37707);
+            }
+
+            if (n == 1 && num < 0)      //проверка на r > 0
+            {
+                Console.WriteLine("Радиус не может быть меньше 0. Конец программы.");
+                Console.ReadKey();
+                Environment.Exit(404);
             }
             return num;
          }
@@ -30,63 +37,83 @@ namespace Hw_2
 
         static double Fun_2(double x, double R)
         {
-            double y = - Math.Sqrt(R * R - x * x);
+            double y;
+            if (R == 0 && x == 0)  //нулевой радиус = точка
+                y = 0;
+            else
+                y = -Math.Sqrt(R * R - x * x);
             return y;
         }
 
         static double Fun_3(double x, double R)
         {
-            double y = Math.Sqrt(R * R - Math.Pow((x - 6), 2));
+            double y;
+            if (R == 0 && x == 6)          //нулевой радиус = точка
+                y = 0;
+            else
+                y = Math.Sqrt(R * R - Math.Pow((x - 6), 2));
             return y;
+        }
+
+        static int Find_fun( double X, double R, ref bool k )
+        {
+            if (R < 3 && X > R && X < (6 - R))        //неопределенность X из-за радиуса
+            {
+                if (k)                  //условие чтобы не выводить одинаковые строки, когда выводятся все значения
+                    return 5;
+                Console.WriteLine("Функция не определена на отрезке {0:N3} < х < {1:N3} ", R, (6 - R));
+                k = true;
+            }
+            else if (X >= -10 && X <= 0)               // прямая
+            {
+                Console.Write("Значение функции:   x =  {0:N3};  y = {1:N3}", X, Fun_1(X));
+                if (R != 3 && X == 0)
+                    Console.WriteLine(",  {0:N3}.", Fun_2(X, R));
+                else
+                    Console.WriteLine(". ");
+            }
+            else if (X >= 0 && X <= 3)           //первая дуга
+            {
+                Console.Write("Значение функции:   x =  {0:N3};  y = {1:N3}", X, Fun_2(X, R));
+                if (R > 3 && X == 3)
+                    Console.WriteLine(",  {0:N3}.", Fun_3(X, R));
+                else
+                    Console.WriteLine(". ");
+            }
+            else                          // вторая дуга
+            {
+                Console.WriteLine("Значение функции:   x =  {0:N3};  y = {1:N3}.", X, Fun_3(X, R));
+            }
+
+            return 37;
         }
 
         static void Main(string[] args)
         {
             Console.Write("Введите радиус:    ");
             string str = Console.ReadLine();
-            double R = Check(ref str);
+            double R = Check(ref str, 1);
 
             Console.Write("Введите значение аргумента :    ");
             str = Console.ReadLine();
             double X_0 = Check(ref str);
             double Y=999, X;
+            bool k = false;
 
-            for (X = -10; X <= 6; X += 1)
+
+            for (X = -10; X <= 6; X = Math.Round(X + 0.2, 3))   //Вывод всех значений
             {
-                    if (R < 3 && X > R && X < (6 - R))
-                    {
-                        Console.WriteLine("Функция не определена на отрезке {0:N3} < х < {1:N3} ", R, (6 - R));
-                    }
-                    else if (X >= -10 && X <= 0)
-                    {
-                        Console.WriteLine("Значение функции:   {0:N3}  {1:N3}", X, Fun_1(X));
-                        if (X_0 >= -10 && X_0 <= 0)
-                            Y = Fun_1(X_0);
-                    }
-                    else if (X > 0 && X <= 3)
-                    {
-                        Console.WriteLine("Значение функции:   {0:N3}  {1:N3}", X, Fun_2(X, R));
-                        if (X_0 > 0 && X_0 <= 3)
-                            Y = Fun_2(X_0, R);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Значение функции:   {0:N3}  {1:N3}", X, Fun_3(X, R));
-                        if (X_0 >= 3 && X_0 <= 6)
-                            Y = Fun_3(X_0, R);
-                    }
+                Find_fun(X, R, ref k);
             }
+            k = false;
 
-
-            Console.WriteLine("------------------------------------------------------");
-            if (R < 3 && X_0 > R && X_0 < (6 - R))
-                Console.WriteLine("Функция не определена на отрезке {0:N3} < х < {1:N3} ", R, (6 - R));
-            else if (X_0 < -10 || X_0 > 6)
+                Console.WriteLine("------------------------------------------------------");     //вывод значения по аргументу
+            if (X_0 < -10 || X_0 > 6)
                 Console.WriteLine("Функция определена на отрезке {0:N3} < х < {1:N3} ", -10, 6);
             else
-                Console.WriteLine("Значение функции в точке {0:N3} :   {1:N3}", X_0, Y);
+                Find_fun(X_0, R, ref k);
 
-            Console.ReadKey();
+                Console.ReadKey();
         }
     }
 }
